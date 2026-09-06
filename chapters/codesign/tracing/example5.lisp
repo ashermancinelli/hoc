317790cb675ecfb32403bd271ec39d7238155a4f
Asher Mancinelli <amancinelli@nvidia.com>
2026-09-06 13:13:23 -0700

    lisp tracing
    Signed-off-by: Asher Mancinelli <amancinelli@nvidia.com>

--- chapters/codesign/tracing/example5.lisp
+++ chapters/codesign/tracing/example5.lisp
@@ -13,19 +13,30 @@
 (defmacro emit (form)
   `(vector-push-extend ,form *ip*))
 
+(defun jit-call (callee &rest args)
+  (let ((sym (gensym "call-result"))
+        )
+    (emit `(,callee ,@args))
+    sym))
+
 (defun jit-for (start end body)
-  (emit `(jit-for ,start ,end :body)))
+  (if
+    (and (integerp start) (integerp end))
+    (loop for i from start below end do (funcall body i))
+    (emit `(jit-for ,start ,end ,body))))
 
 (defun rewrite-loop (loop-form)
   (destructuring-bind
     (_l _f i _f start _b end _d body)
     loop-form
     (format t "~a from ~a to ~a ~a~%" (type-of i) (type-of start) (type-of end) body)
-    `(jit-for
-       ,(jit-compile start)
-       ,(jit-compile end)
-       (lambda (,i)
-         ,(jit-compile body)))))
+    (let ((*ir* (make-ir))
+          (body-ir (jit-compile body)))
+      `(jit-for
+         ,(jit-compile start)
+         ,(jit-compile end)
+         (lambda (,i) ,body-ir)
+         ))))
 
 (defmacro with-ip (ip &rest body)
   `(let ((*outer* *ip* *ip* ip))
@@ -34,15 +45,18 @@
 
 (defun set-item (arr idx val)
   (emit `(set-item ,arr ,idx ,val)))
+
 (defun get-item (arr idx)
   (let ((out (gensym "get-item")))
-    (emit `(setq ,out (get-item ,arr ,idx)))))
+    (emit `(setq ,out (get-item ,arr ,idx)))
+    out))
 
 (defun do-jit-compile (form)
   (cond
     ((atom form) form)            ; skip atoms
     ((eq (car form) 'quote) form) ; skip quotes
     ((eq (car form) 'loop) (rewrite-loop form))
+    ((eq (car form) '+) `(jit-call ,@form))
     ((eq (car form) 'setf)
      (destructuring-bind (_setf (_aref arr idx) val) form
        `(set-item
@@ -58,19 +72,19 @@
 (defun jit-compile (form)
   (let ((after (do-jit-compile form)))
     (if (not (equal form after))
-      (format t "~a~%~%"
-              `((before ,form)
-                (after ,after))))
+      (format t "--- ~a~%+++ ~a~%~%" form after))
     after))
 
 (defmacro trace-jit (name args &body body)
   (format t "JIT-compiling ~a~%~a~%" name `(defun ,name ,args ,body))
   (let* ((*ip* (make-ir))
+         (arg-syms (mapcar #'(lambda (_) (quote (gensym "jit-arg"))) args))
          (fn (gensym "ast-compiled"))
          (compiled
            `(defun ,name ,args
-              (let ((,fn (lambda ,args ,@(mapcar #'jit-compile body))))
-                (funcall ,fn ,@args)
+              (let ((*ip* (make-ir))
+                    (,fn (lambda ,args ,@(mapcar #'jit-compile body))))
+                (funcall ,fn ,@arg-syms)
                 *ip*))))
     (format t "AST-preprocessed function:~%~a~%" compiled)
     compiled))
@@ -81,9 +95,6 @@
         (setf (aref a i)
               (+ (elt a i) i))))
 (let* ((N 5)
-       (a (make-array N)))
-  (foo a N)
-  (format t
-          "Result: ~a~%IR: ~a~%" a *ip*)
-  (pprint a)
-  (pprint *ip*))
+       (a (make-array N))
+       (ir (foo a N)))
+  (format t "Result: ~a~%IR: ~a~%" a ir))
